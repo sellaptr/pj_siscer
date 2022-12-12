3f9d3396d4696aed5f9d4a9d17b219b3708e6adc
mahbubi69 <muhammadmahbubi495@gmail.com>
2022-12-12 11:41:43 +0700

    perbaikan sql

--- transkrip.sql
+++ transkrip.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.4deb1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 10 Des 2022 pada 12.29
--- Versi server: 10.4.25-MariaDB
--- Versi PHP: 8.1.10
+-- Host: localhost:3306
+-- Generation Time: Dec 12, 2022 at 11:39 AM
+-- Server version: 8.0.31-0ubuntu2
+-- PHP Version: 8.1.7-1ubuntu3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,33 +24,153 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `mahasiswa`
+-- Table structure for table `mahasiswa`
 --
 
 CREATE TABLE `mahasiswa` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nama` varchar(255) NOT NULL,
   `nim` varchar(255) NOT NULL,
   `ttl` varchar(255) NOT NULL,
   `alamat` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `mahasiswa`
+-- Dumping data for table `mahasiswa`
 --
 
 INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `ttl`, `alamat`) VALUES
-(0, 'Mahbubi', 'E3123432', '01-01-2000', 'Bondowoso');
+(1, 'Mahbubi', 'E3123432', '01-01-2000', 'Bondowoso'),
+(3, '324', '234', '23', '23'),
+(4, 'we', 'we', 'we', 'we'),
+(5, 'we', 'sd', 'sd', 'sd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matakuliah`
+--
+
+CREATE TABLE `matakuliah` (
+  `id` int NOT NULL,
+  `nama_mkl` varchar(255) NOT NULL,
+  `kode_mkl` varchar(25) NOT NULL,
+  `pengampu` varchar(255) NOT NULL,
+  `nilai_matkul` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `matakuliah`
+--
+
+INSERT INTO `matakuliah` (`id`, `nama_mkl`, `kode_mkl`, `pengampu`, `nilai_matkul`) VALUES
+(1, 'Sistem Cerdas', 'TI1', 'Trismayanti Dwi Puspitasari', 0),
+(11, 'Workshop Sistem Tertanam', 'TI2', 'I Gede Wiryawan', 0),
+(12, 'Logika dan Algoritma', 'TI3', 'Adi Heru Utomo', 0),
+(13, 'Matematika Diskrit', 'TI4', 'Zilvanhisna Emka Fitri', 0),
+(14, 'Workshop Sistem Cerdas', 'TI5', 'Aji Seto Arifianto', 0),
+(22, 'Workshop Pengolahan Citra & Vision', 'TI6', 'I Putu Dodi Lesmana', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `id` int NOT NULL,
+  `jumlah_nilai` float NOT NULL,
+  `nilai_rata` float NOT NULL,
+  `id_matakuliah` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
+(1, 'dosen@dsn', 'dosen123', 'Dosen'),
+(2, 'mahasiswa@mhs', 'mahasiswa123', 'Mahasiswa');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `mahasiswa`
+-- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `matakuliah`
+--
+ALTER TABLE `matakuliah`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_matakuliah` (`id_matakuliah`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `matakuliah`
+--
+ALTER TABLE `matakuliah`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `nilai`
+--
+ALTER TABLE `nilai`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`id_matakuliah`) REFERENCES `matakuliah` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
